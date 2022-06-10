a805704030caf93b9241674aff2d2ac323305880
Diamond Nicholas <diamondnicholas@apples-MacBook-Pro.local>
2022-06-10 08:46:52 +0100

    create model, and constructors

--- app.py
+++ app.py
@@ -12,3 +12,13 @@ app.config['SQLAlCHEMY_DATABASE_URI']= os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 
 
+class Item(db.model):
+ id = db.Column(db.integer, primary_key=True)
+ title= db.Column(db.String(80), unique=True, nullable=False)
+ content= db.Column(db.String(120), unique=True, nullable=False)
+
+ def __init__(self, title, content):
+  self.title = title
+  self.content = content
+
+db.create_all()
