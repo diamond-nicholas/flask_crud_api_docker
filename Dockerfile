897142d419773f54e1b6616a96ff8c6d36dcf2ac
Diamond Nicholas <diamondnicholas@apples-MacBook-Pro.local>
2022-06-10 16:04:47 +0100

    containerise application with docker

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM python:3.9.13
+
+FROM python:3.6-slim-buster
 
 COPY requirement.txt .
 
@@ -6,6 +7,6 @@ RUN pip3 install -r requirement.txt
 
 COPY . .
 
-EXPOSE 80
+# EXPOSE 80
 
-CMD ['flask', 'run', '--host-0.0.0.0', '--port-80']
+CMD ["flask", "run", "--host=0.0.0.0", "--port=80"]
--- app.py
+++ app.py
@@ -13,8 +13,8 @@ app.config['SQLAlCHEMY_DATABASE_URI']= os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 
 
-class Item(db.model):
- id = db.Column(db.integer, primary_key=True)
+class Item(db.Model):
+ id = db.Column(db.Integer, primary_key=True)
  title= db.Column(db.String(80), unique=True, nullable=False)
  content= db.Column(db.String(120), unique=True, nullable=False)
 
@@ -22,42 +22,44 @@ class Item(db.model):
   self.title = title
   self.content = content
 
- db.create_all()
+
 
 # create endpoints
 
- @app.route('/items/<db>', methods=['GET'])
- def get_item(id):
-  item = Item.query.get(id)
+@app.route('/items/<db>', methods=['GET'])
+def get_item(id):
+ item = Item.query.get(id)
+ del item.__dict__['_sa_instance_state']
+ return jsonify(item.__dict__)
+
+@app.route('/items/', methods=['GET'])
+def get_items():
+ items = []
+ for item in db.session.query(Item).all():
   del item.__dict__['_sa_instance_state']
-  return jsonify(item.__dict__)
+  items.append(item.__dict__)
+ return json(items)
 
- @app.route('/items/', methods=['GET'])
- def get_items():
-  items = []
-  for item in db.session.query(Item).all():
-   del item.__dict__['_sa_instance_state']
-   items.append(item.__dict__)
-  return json(items)
+@app.route('/items', methods=['POST'])
+def create_item():
+ body = request.get_json()
+ db.session.add(Item(body['title'], body['content']))
+ db.session.commit()
+ return 'Item created'
 
- @app.route('/items', methods=['POST'])
- def create_item():
-  body = request.get_json()
-  db.session.add(Item(body['title'], body['content']))
-  db.session.commit()
-  return 'Item created'
-
- @app.route('/items/<id>', methods=['PUT'])
- def update_item(id):
-  body = request.get_json()
-  db.session.query(Item).filter_by(id=id).update(
+@app.route('/items/<id>', methods=['PUT'])
+def update_item(id):
+ body = request.get_json()
+ db.session.query(Item).filter_by(id=id).update(
    dict(title=body['title'], content=body['content'])
   )
-  db.session.commit()
-  return 'Item updated'
+ db.session.commit()
+ return 'Item updated'
 
- @app.route('items/<id>', methods=['DELETE'])
- def delete_item(id):
-  db.session.query(Item).filter_by(id=id).delete()
-  db.session.commit()
-  return "item deleted"
+@app.route('/items/<id>', methods=['DELETE'])
+def delete_item(id):
+ db.session.query(Item).filter_by(id=id).delete()
+ db.session.commit()
+ return "item deleted"
+
+db.create_all()
